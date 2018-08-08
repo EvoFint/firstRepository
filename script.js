fd3ef3cc3f88fcdfec9ed64692cb48833c8dd07c
Roman <evofint@gmail.com>
2018-08-08 15:23:32 +0700

    easy tasks

---
+++ script.js
@@ -0,0 +1,21 @@
+/* Task 2 */
+var budget = prompt("Ваш Бюджет на месяц?");
+var nameShop = prompt("Название вашего магазина?");
+
+/* Task 3 */
+var mainList = {
+    money: budget,
+    name: nameShop,
+    shopGoods: [],
+    employers: {},
+    open
+}
+
+/* Task 4 */
+mainList.shopGoods.push(prompt("Какой тип товаров будем продавать?"));
+mainList.shopGoods.push(prompt("Какой тип товаров будем продавать?"));
+mainList.shopGoods.push(prompt("Какой тип товаров будем продавать?"));
+
+/* Task 5 */
+var oneDayBudget = mainList.money / 30;
+alert('Ваш бюджет на 1 день составляет: ' + oneDayBudget);
